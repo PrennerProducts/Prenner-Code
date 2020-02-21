3790047f43040a515dfd3f32a30c8f3d1f86dc0c
PrennerProducts <prenner.products@gmail.com>
2020-02-21 18:34:17 +0100

    aufiloden

--- Pi als Grafik.py
+++ Pi als Grafik.py
@@ -1,1 +1,25 @@
-print("hallo Lukas")
+import turtle as tu
+lines = 100_000
+
+with open("1million.txt", "r") as f:
+    pi = f.read()
+
+tu.mode("logo")
+tu.tracer(False)
+tu.screensize(6000, 6000, 'black')
+tu.colormode(255)
+
+
+
+for n in range(lines):
+    color=int(n/(lines/255))
+    tu.pencolor(255,255-color,color)
+    zahl = int(pi[n])
+    rotation = zahl *36
+    tu.setheading(rotation)
+    tu.forward(5)
+    if n % 10_000 == 0:
+        tu.update()
+
+tu.getcanvas().postscript(file="pi_Picture.ps")
+tu.done()
---
+++ if abfrage.py
@@ -0,0 +1,18 @@
+x = 52
+y = True
+
+if x < 50:
+    print('x kleiner 50')
+    x = 0
+
+elif x ==50:
+    print('x = 50')
+
+elif x == 52:
+    print('sonderfall')
+    
+
+else:
+    print('x größer gleich 50')
+
+print('hello world!')
