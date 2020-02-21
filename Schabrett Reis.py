3317b6b9b750b2d0feb0d3bc77751c803c5ccc13
PrennerProducts <prenner.products@gmail.com>
2020-02-21 14:32:56 +0100

    Teständerung 1xPrint

--- Schabrett Reis.py
+++ Schabrett Reis.py
@@ -5,7 +5,7 @@ for feld in range(64):
     print("feld {}. = {:>30,} Reiskörne und damint insgesammt {:>30,} Reiskörner" \
         .format(feld+1,reiskorn,summe))
 gewicht = summe * 0.02 /1000 /1000
-print()
+
 print()
 print("Wenn ein Reiskorn 0,02 Gramm wiegt, wiegen die gesamten")
 print("Reiskörner {:,.0f} Tonnen".format(gewicht))
