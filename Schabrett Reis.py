0e6ea0134071a1d20f4de57ef89d8bcc48104029
PrennerProducts <prenner.products@gmail.com>
2020-02-21 15:03:05 +0100

    Änderung Grafikanzeige

--- Schabrett Reis.py
+++ Schabrett Reis.py
@@ -1,11 +1,19 @@
+import matplotlib.pyplot as plt
+
 summe = 0
+feldliste = []
+
 for feld in range(64):
     reiskorn = 2**feld
-    summe = summe + reiskorn
-    print("feld {}. = {:>30,} Reiskörne und damint insgesammt {:>30,} Reiskörner" \
-        .format(feld+1,reiskorn,summe))
+    feldliste.append(reiskorn)
+    summe += reiskorn
+    print(f"Feld {feld+1}. = {reiskorn:>30,} Reiskörner und damint insgesammt" 
+         f"{summe:>30,} Reiskörner")
+    
 gewicht = summe * 0.02 /1000 /1000
 
 print()
-print("Wenn ein Reiskorn 0,02 Gramm wiegt, wiegen die gesamten")
-print("Reiskörner {:,.0f} Tonnen".format(gewicht))
+print("Wenn ein Reiskorn 0,02 Gramm wiegt, wiegen die gesamten"
+f" Reiskörner {gewicht:,.0f} Tonnen")
+plt.plot(feldliste)
+plt.show()
